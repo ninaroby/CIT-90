e6e7d5559fc4d3670d136cdd53e004973a9e854d
ninaroby <ninaroby.usa@gmail.com>
2016-09-21 20:57:04 -0700

    abstracting serve function

---
+++ week-6/task-4/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		io.WriteString(conn, "Hi you!")
+
+		serve(conn)
+	}
+}
+
+func serve(c net.Conn) {
+	defer c.Close()
+	scanner := bufio.NewScanner(c)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		line := scanner.Text()
+		fmt.Fprintln(c, line)
+		if line == "" {
+			break
+		}
+	}
+
+	io.WriteString(c, "haaaaa")
+}
