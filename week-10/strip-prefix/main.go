fe8b00c4a05c150ea4d7b34047a14d224835a260
ninaroby <ninaroby.usa@gmail.com>
2016-10-19 20:21:47 -0700

    Strip Prefix

---
+++ week-10/strip-prefix/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", cool)
+	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
+	http.ListenAndServe(":8080", nil)
+}
+
+func cool(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `<img width="800px" src="/assets/cool-cat.jpg">`)
+}
